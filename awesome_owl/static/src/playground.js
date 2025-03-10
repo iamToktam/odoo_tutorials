ced5f9f1af88235cee9df128f25b46bf55f24e6b
dybug <to.jafarian83@gmail.com>
2025-03-10 15:11:37 +0330

    [FIX] create owl components (step 1 to 4)

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,7 +1,16 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, markup } from "@odoo/owl";
+import { Counter } from "./counter/counter";
+import { Card } from "./card/card";
 
 export class Playground extends Component {
-    static template = "awesome_owl.playground";
+    static template = "awesome_owl.Playground";
+    static components = { Counter, Card };
+
+    setup() {
+        this.safeContent = markup("<em>safe HTML</em>");
+        this.unsafeContent = "<strong>unsafe HTML</strong>";
+    }
+
 }
