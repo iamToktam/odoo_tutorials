fa87403f9269ad3699ae4c61b398cc9aca7c30d4
dybug <to.jafarian83@gmail.com>
2025-03-27 14:59:23 +0330

    [IMP] Build a Clicker game : chapter 1 to 9

---
+++ awesome_clicker/static/src/client_action/client_action.js
@@ -0,0 +1,17 @@
+import {Component, useState} from "@odoo/owl";
+import {registry} from "@web/core/registry";
+import {useService} from "@web/core/utils/hooks";
+ import { ClickerValue } from "../clicker_value/clicker_value";
+
+class ClickerClientAction extends Component {
+	static template = "awesome_clicker.ClickerClientAction";
+	static props = ["*"];
+
+	static components = {ClickerValue};
+
+	setup() {
+		this.clickService = useState(useService("awesome_clicker.clicker"));
+	}
+}
+
+registry.category("actions").add("awesome_clicker.client_action", ClickerClientAction);
