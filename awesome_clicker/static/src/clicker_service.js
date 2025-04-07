2e4cfd578b95280cccf5486190bd0fc96e21bba4
dybug <to.jafarian83@gmail.com>
2025-04-07 09:57:51 +0330

    [IMP] Build a Clicker game : chapter11, Add BigBots for 5000 clicks

--- awesome_clicker/static/src/clicker_service.js
+++ awesome_clicker/static/src/clicker_service.js
@@ -2,28 +2,19 @@ import {registry} from "@web/core/registry";
 import {ClickerModel} from "./clicker_model";
 
 const clickerService = {
-    dependencies: ["effect"],
-    start(env, services) {
+	dependencies: ["effect"],
+	start(env, services) {
+		const clickerModel = new ClickerModel();
 
-        const model = new ClickerModel();
-
-        document.addEventListener("click", () => model.addClick(), true);
-
-        setInterval(() => {
-            model.tick();
-        }, 10000);
-
-        const bus = model.bus;
-
-        bus.addEventListener("MILESTONE_1k", () => {
-            services.effect.add({
-                message: "Milestone reached! You can now buy clickbots",
-                type: "rainbow_man",
-            });
-        });
-
-        return model;
-    },
+		const bus = clickerModel.bus
+		bus.addEventListener("MILESTONE", (ev) => {
+			services.effect.add({
+				message: `Milestone reached! You can now buy ${ev.detail.unlock}`,
+				type: "rainbow_man",
+			});
+		});
+		return clickerModel;
+	},
 };
 
 registry.category("services").add("awesome_clicker.clicker", clickerService);
