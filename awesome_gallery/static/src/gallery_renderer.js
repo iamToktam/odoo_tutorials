d055d6a5dd3bc098923ee340334cc9b9605f6363
dybug <to.jafarian83@gmail.com>
2025-04-22 09:31:01 +0330

    [IMP] Create a Gallery View: 8. Display images

--- awesome_gallery/static/src/gallery_renderer.js
+++ awesome_gallery/static/src/gallery_renderer.js
@@ -1,8 +1,12 @@
 import {Component} from "@odoo/owl";
 import {GalleryModel} from "./gallery_model";
+import {GalleryImage} from "./gallery_image";
+
 
 export class GalleryRenderer extends Component {
+	static components = {GalleryImage}
 	static template = "awesome_gallery.GalleryRenderer";
+
 	static props = {
 		model: GalleryModel,
 	}
