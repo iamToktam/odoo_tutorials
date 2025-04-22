892b346681a3cc5edc14fcb5d747e465159e8a63
dybug <to.jafarian83@gmail.com>
2025-04-22 16:44:56 +0330

    [IMP] Create a Gallery View: 14. Advanced tooltip template

--- awesome_gallery/static/src/gallery_arch_parser.js
+++ awesome_gallery/static/src/gallery_arch_parser.js
@@ -1,12 +1,32 @@
+import {visitXML} from "@web/core/utils/xml";
+// کمک می‌کنه ساختار XML رو به صورت درختی بررسی کنیم
+
 export class GalleryArchParser {
 	parse(xmlDoc) {
 		const imageField = xmlDoc.getAttribute("image_field");
 		const limit = xmlDoc.getAttribute("limit") || 80;
-		const tooltipField = xmlDoc.getAttribute("tooltip_field");
+
+		// آرایه خالی برای نگه‌داشتن نام فیلدها
+		const fieldsForTooltip = [];
+
+		let tooltipTemplate = undefined;
+
+		// این تابع روی همه‌ی تگ‌های XML می‌چرخه و هر تگ رو به صورت جدا میفرسته به اون تابع داخلی
+		visitXML(xmlDoc, (node) => {
+			if (node.tagName === "field") {
+				fieldsForTooltip.push(node.getAttribute("name"));
+			}
+			if (node.tagName === "tooltip-template") {
+				tooltipTemplate = node;
+			}
+		})
+
 		return {
 			imageField,
 			limit,
-			tooltipField,
+
+			fieldsForTooltip,
+			tooltipTemplate,
 		}
 	}
 }
