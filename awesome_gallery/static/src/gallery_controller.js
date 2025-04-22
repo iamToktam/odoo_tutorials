f32ac21636a5ea49ac6a3b97141049bfad42859b
dybug <to.jafarian83@gmail.com>
2025-04-22 11:09:42 +0330

    [IMP] Create a Gallery View: 10. Add an optional tooltip

--- awesome_gallery/static/src/gallery_controller.js
+++ awesome_gallery/static/src/gallery_controller.js
@@ -9,38 +9,39 @@ import {standardViewProps} from "@web/views/standard_view_props";
 
 
 export class GalleryController extends Component {
-    static template = "awesome_gallery.GalleryController";
-    static props = {
-        ...standardViewProps,
-        archInfo: Object,
-        Model: Function,
-        Renderer: Function,
+	static template = "awesome_gallery.GalleryController";
+	static props = {
+		...standardViewProps,
+		archInfo: Object,
+		Model: Function,
+		Renderer: Function,
 
-    };
-    static components = {Layout};
+	};
+	static components = {Layout};
 
-    setup() {
-        // ارتباط با سرویس برای خواندن و فرستادن داده
-        this.orm = useService("orm");
+	setup() {
+		// ارتباط با سرویس برای خواندن و فرستادن داده
+		this.orm = useService("orm");
 
-        this.model = useState(
-            new this.props.Model(
-                this.orm,
-                this.props.resModel,
-                this.props.archInfo,
-            )
-        );
+		this.model = useState(
+			new this.props.Model(
+				this.orm,
+				this.props.resModel,
+				this.props.fields,
+				this.props.archInfo,
+			)
+		);
 
-        // بارگذاری اولیه
-        onWillStart(async () => {
-            await this.model.load(this.props.domain);
-        });
+		// بارگذاری اولیه
+		onWillStart(async () => {
+			await this.model.load(this.props.domain);
+		});
 
-        onWillUpdateProps(async (nextProps) => {
-            if (JSON.stringify(nextProps.domain) !== JSON.stringify(this.props.domain)) {
-                await this.model.load(nextProps.domain);
-            }
-        });
-    }
+		onWillUpdateProps(async (nextProps) => {
+			if (JSON.stringify(nextProps.domain) !== JSON.stringify(this.props.domain)) {
+				await this.model.load(nextProps.domain);
+			}
+		});
+	}
 
 }
