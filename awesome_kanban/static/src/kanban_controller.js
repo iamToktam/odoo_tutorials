d54557a5f160b493e6d50af460cc370820f42685
dybug <to.jafarian83@gmail.com>
2025-04-28 09:50:56 +0330

    [IMP] 2. Create a CustomerList component

--- awesome_kanban/static/src/kanban_controller.js
+++ awesome_kanban/static/src/kanban_controller.js
@@ -1,4 +1,18 @@
 import {KanbanController} from "@web/views/kanban/kanban_controller";
+import {CustomerList} from "./customer_list/customer_list";
 
 export class AwesomeKanbanController extends KanbanController {
+
+	 // همه‌ی کامپوننت‌های اصلی Kanban رو نگه داری ...KanbanController.components
+
+	static components = {...KanbanController.components, CustomerList}
+	static template = "awesome_kanban.AwesomeKanbanController";
+
+	setup() {
+		super.setup();
+	}
+
+	selectCustomer(customer_id) {
+		console.log(customer_id);
+	}
 }
