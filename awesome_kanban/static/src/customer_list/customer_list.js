ccf6fb38ebc2db7e2a6a190f32b363fcb8ead874
dybug <to.jafarian83@gmail.com>
2025-04-28 10:35:39 +0330

    [IMP] 3. Load and display data

--- awesome_kanban/static/src/customer_list/customer_list.js
+++ awesome_kanban/static/src/customer_list/customer_list.js
@@ -1,4 +1,5 @@
-import {Component} from "@odoo/owl";
+import {Component, onWillStart} from "@odoo/owl";
+import {useService} from "@web/core/utils/hooks";
 
 export class CustomerList extends Component {
 	static template = "awesome_kanban.CustomerList";
@@ -7,4 +8,12 @@ export class CustomerList extends Component {
 			type: Function,
 		},
 	};
+
+	// هرچی میخوای قبل از نمایش آماده کن
+	setup() {
+		this.orm = useService("orm");
+		onWillStart(async () => {
+			this.partners = await this.orm.searchRead("res.partner", [], ["display_name"]);
+		})
+	}
 }
