742ce99dd078f701b43d3adf6ce8cc58f94bcc51
dybug <to.jafarian83@gmail.com>
2025-04-28 11:05:42 +0330

    [IMP] 5. Only display customers which have an active order

--- awesome_kanban/static/src/customer_list/customer_list.js
+++ awesome_kanban/static/src/customer_list/customer_list.js
@@ -1,5 +1,6 @@
-import {Component, onWillStart} from "@odoo/owl";
 import {useService} from "@web/core/utils/hooks";
+import {Component, onWillStart, useState} from "@odoo/owl";
+import {KeepLast} from "@web/core/utils/concurrency";
 
 export class CustomerList extends Component {
 	static template = "awesome_kanban.CustomerList";
@@ -12,8 +13,21 @@ export class CustomerList extends Component {
 	// هرچی میخوای قبل از نمایش آماده کن
 	setup() {
 		this.orm = useService("orm");
+		this.partners = useState({data: []});
+		this.keepLast = new KeepLast();
+
 		onWillStart(async () => {
-			this.partners = await this.orm.searchRead("res.partner", [], ["display_name"]);
+			this.partners.data = await this.loadCustomers([]);
 		})
+	}
+
+	async onChangeActiveCustomers(ev) {
+		const checked = ev.target.checked;
+		const domain = checked ? [["opportunity_ids", "!=", false]] : [];
+		this.partners.data = await this.keepLast.add(this.loadCustomers(domain));
+	}
+
+	loadCustomers(domain) {
+		return this.orm.searchRead("res.partner", domain, ["display_name"]);
 	}
 }
