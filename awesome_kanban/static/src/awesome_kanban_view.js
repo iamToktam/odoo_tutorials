68af587800f54ebfa017443ab8bdae324d7c6f1f
dybug <to.jafarian83@gmail.com>
2025-04-28 08:51:43 +0330

    [IMP] Create a new kanban view

--- awesome_kanban/static/src/awesome_kanban_view.js
+++ awesome_kanban/static/src/awesome_kanban_view.js
@@ -1,3 +1,11 @@
 /** @odoo-module */
+import {kanbanView} from "@web/views/kanban/kanban_view";
+import {registry} from "@web/core/registry";
+import {AwesomeKanbanController} from "./kanban_controller";
 
-// TODO: Define here your AwesomeKanban view
+const awesomeKanbanController = {
+	...kanbanView,
+	Controller: AwesomeKanbanController,
+};
+
+registry.category("views").add("awesome_kanban", awesomeKanbanController);
---
+++ awesome_kanban/static/src/kanban_controller.js
@@ -0,0 +1,4 @@
+import {KanbanController} from "@web/views/kanban/kanban_controller";
+
+export class AwesomeKanbanController extends KanbanController {
+}
